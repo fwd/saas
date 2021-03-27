12776a21b86add29938de458d0433b72abadc816
Esteban <me@estebansanti.com>
2021-03-27 14:43:16 -0400

    Update auth.js

--- plugins/utilities/auth.js
+++ plugins/utilities/auth.js
@@ -201,6 +201,10 @@ module.exports = (config) => {
 				var host = (req.get('host') == 'localhost' ? 'http://' : 'https://') + req.get('host')
 				var resetUrl = host + '?token=' + reset.id + '/#/reset'
 
+				if (config.business && config.business.logo && !config.business.logo.includes('http')) {
+					config.business.logo = `${host}${config.business.logo}`
+				}
+
 				var email = {
 					to: username,
 					subject: 'Password Reset',
